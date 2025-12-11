ec1fc542ae29072999def8285653d9ac30ccd9c7
DanyRoland <eldanielitu@gmail.com>
2025-12-11 09:15:34 -0400

    permisos + rutas de fotos
    necesito un lugar donde guardar mis imágenes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
 
     implementation("androidx.navigation:navigation-compose:2.9.6")
+    implementation("com.google.ai.client.generativeai:generativeai:0.9.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 }
