23a3a9d61f067a962f7c683b55ce5ad569aba4db
DanyRoland <eldanielitu@gmail.com>
2025-12-14 08:47:40 -0400

    ImageElement diseñado
    debería ser utilizable en todas las instancias donde hago uso de imagenes, pero aún falta la implementación de esta, a ver qué tal

--- app/src/main/java/com/example/flowerdexapp/ui/globalElements.kt
+++ app/src/main/java/com/example/flowerdexapp/ui/globalElements.kt
@@ -1,6 +1,22 @@
 package com.example.flowerdexapp.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.flowerdexapp.R
 
 @Composable
 fun ButtonElement(){
@@ -13,6 +29,47 @@ fun SmallInfoElement(){
 }
 
 @Composable
-fun ImageElement(){
-
+fun ImageElement(
+    imageUri: String?,
+    modifier: Modifier = Modifier,
+    sizeDp: Int = 200,
+    borderRadiusDp: Int = 12,
+    borderWidthDp: Int = 2
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(1f),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(sizeDp.dp)
+                .clip(RoundedCornerShape(borderRadiusDp.dp))
+        ) {
+            if(imageUri != null) {
+                AsyncImage(
+                    model = imageUri,
+                    contentDescription = "Imagen",
+                    placeholder = painterResource(id = R.drawable.placeholder),
+                    error = painterResource(id = R.drawable.placeholder),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.matchParentSize()
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.placeholder),
+                    contentDescription = "Placeholder",
+                    modifier = Modifier.matchParentSize(),
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .border(borderWidthDp.dp,
+                        Color.Black,
+                        RoundedCornerShape(borderRadiusDp.dp))
+            )
+        }
+    }
 }
