ed76476aea9b356573454f0b193add8c2f73d9f0
DanyRoland <eldanielitu@gmail.com>
2025-12-14 08:07:37 -0400

    Definición de elementos reutilizables

---
+++ app/src/main/java/com/example/flowerdexapp/ui/globalElements.kt
@@ -0,0 +1,18 @@
+package com.example.flowerdexapp.ui
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ButtonElement(){
+
+}
+
+@Composable
+fun SmallInfoElement(){
+
+}
+
+@Composable
+fun ImageElement(){
+
+}
